85a7df416dd4587279a8993ae089fa0100388133
manishbar <MANISHMICKEYBARMAN@GMAIL.COM>
2022-12-15 01:32:58 +0530

    update mongo

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+app.listen(8080, () => {
+    console.log('Serving on port 8080')
 })
